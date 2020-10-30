77f72f9b5ab051b57cdd29fe94e0aed35e254338
majaarvola <majaarvola@gmail.com>
2020-10-30 10:24:13 +0100

    Added code skeleton for preprocessing and feature extraction

---
+++ feature_extraction.py
@@ -0,0 +1,17 @@
+
+def extract_features_from_image(imagePath, paramsPath):
+    '''
+    ACTION: Extract radiomic features from one image given mask and parameters
+    INPUT: imagePath, paramsPath
+    OUTPUT: dictionary
+    '''
+    pass
+
+def extract_features_from_all(folderPath, paramsPath):
+    '''
+    ACTION: Loops through all patients, extract features and store the content in a table
+    INPUT: folder path and parameter path
+    OUTPUT: table where each row corresponds to one patient and each column 
+            corresponds to one feature (CHECK THAT THIS IS VALID INPUT FOR NEXT STEP IN PIPELINE)
+    '''
+    pass
---
+++ image_preprocessing.py
@@ -0,0 +1,56 @@
+
+import numpy as np
+import cv2
+
+
+def create_mask(inputFile, outputFile, showResult=True):
+    """ Generates a black and white mask of the input image
+    The white area corresponds to green markings in the 
+    file including any interior points and the rest is black.
+    """
+
+    # Read image (if it exists) and make copy for comparison
+    if cv2.haveImageReader(inputFile):
+        original_image = cv2.imread(inputFile)
+    else:
+        print(f"Failed to read input file at {inputFile}")
+        return
+    image = original_image.copy()
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    # Define color range and mask everything within range
+    lower = np.array([5, 10, 5], dtype="uint8")
+    upper = np.array([230, 255, 230], dtype="uint8")
+    mask = cv2.inRange(image, lower, upper)
+
+    # Add interior points to mask
+    cnts = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+    mask = cv2.fillPoly(mask, cnts, (255, 255, 255))
+
+    # save the output
+    if not cv2.imwrite(outputFile, mask):
+        print(f"Failed to write output file at {outputFile}")
+
+    if showResult:
+        # Apply mask to original image and show images for visual verification
+        result = cv2.bitwise_and(original_image, original_image, mask=mask)
+        cv2.imshow('intial', original_image)
+        cv2.imshow('mask', mask)
+        cv2.imshow('result', result)
+        cv2.waitKey(1000)
+
+
+def create_masks(folderPath):
+    ''' Action: Creates a mask for every image in folder and subfolders. 
+    The masks are named with the same name as the original images but with an extension "_mask"
+    Input: folderpath
+    '''
+    pass
+
+if __name__ == "__main__":
+    pass
+    # #example run:
+    # inputFile = "../data/Pat12T2M/Pat12T2M45.tiff"
+    # outputFile = "../data/Pat12T2M_masked/Pat12T2M45_masked.tiff"
+    # create_mask(inputFile, outputFile, showResult=True)
