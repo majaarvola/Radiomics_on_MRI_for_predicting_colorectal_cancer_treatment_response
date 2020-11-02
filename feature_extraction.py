bb37bb9b099368e2c4b623604742cb3d2a2345ee
emil96aberg <emil.aberg96@gmail.com>
2020-11-02 16:57:23 +0100

    Update feature_extraction.py
    Added code for extracting features for one image and printing the results

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,3 +1,6 @@
+import cv2 
+import os
+from radiomics import featureextractor
 
 def extract_features_from_image(imagePath, paramsPath):
     """
@@ -5,13 +8,37 @@ def extract_features_from_image(imagePath, paramsPath):
     INPUT: imagePath, paramsPath
     OUTPUT: dictionary
     """
-    pass
+
+    img = imagePath + ".nrrd"
+    mask = imagePath + "_mask.nrrd"
+
+    if not os.path.isfile(img):
+        raise IOError('File does not exist: %s' % img)
+    elif not os.path.isfile(mask):
+        raise IOError('File does not exist: %s' % mask)
+    elif not os.path.isfile(paramsPath):
+        raise IOError('File does not exist: %s' % paramsPath)
+
+    extractor = featureextractor.RadiomicsFeatureExtractor(paramsPath)
+    results = extractor.execute(img, mask, label=255)
+    return results
+
 
 def extract_features_from_all(folderPath, paramsPath):
     """
     ACTION: Loops through all patients, extract features and store the content in a table
     INPUT: folder path and parameter path
     OUTPUT: table where each row corresponds to one patient and each column
-            corresponds to one feature (CHECK THAT THIS IS VALID INPUT FOR NEXT STEP IN PIPELINE)
+            corresponds to one feature 
+            (CHECK THAT THIS IS VALID INPUT FOR NEXT STEP IN PIPELINE)
     """
     pass
+
+def print_features(features):
+    """
+    ACTION: Print the radiomics features
+    INPUT: dictionary with radiomics features
+    """
+    print("Calculated features")
+    for key, value in features.items():
+        print("\t", key, ":", value)
