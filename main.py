7aae466a5538ae4492fcc1b3c3eb898f162ce3de
emil96aberg <emil.aberg96@gmail.com>
2020-11-10 17:15:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,28 +4,16 @@ import feature_extraction as fextr
 # Extract label data from patients (either here or in seperate file)
 
 
-# # Settings for feature extraction
-# # paramsPath = "Params.yaml"
-# paramsPath = "MR_2D_extraction.yaml"
+# Settings for feature extraction
+paramsPath = "Params.yaml"
 
-# # Given images
-# inputFileNameM = "../../patient_data/10T2M" # With green line
-# inputFileNameU = "../../patient_data/10T2U" # Without green line
-# fileExt = ".tiff"
+# dirPath = "../../patient_data"
+# imgpr.create_masks_and_nrrds(dirPath, overWrite=False)
 
-# # Create a mask
-# maskFileName = "../../patient_data/10T2M_mask"
-# showResult = False
-# imgpr.create_mask(inputFileNameM + fileExt, maskFileName + fileExt, showResult)
+dirMaskPath = "../../patient_data/Pat14/Pat14T2M_mask"
+# imgpr.create_3d_nrrd(dirMaskPath)
+dirImgPath = "../../patient_data/Pat14/Pat14T2U"
+# imgpr.create_3d_nrrd(dirImgPath)
 
-# imgpr.img2nrrd(maskFileName)
-# imgpr.img2nrrd(inputFileNameU)
-
-# features = fextr.extract_features_from_image(inputFileNameU, maskFileName, paramsPath)
-
-# fextr.print_features(features)
-# print("Total number of features: ", len(features))
-
-
-dirPath = "..\..\patient_data"
-imgpr.create_masks_and_nrrds(dirPath, overWrite=False)
+results = fextr.extract_features_from_image(dirImgPath, dirMaskPath, paramsPath) 
+fextr.print_features(results)
