95d9852b0b2a9213a63f0896601f16b103e612a4
emil96aberg <emil.aberg96@gmail.com>
2020-11-13 14:47:59 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,22 @@
 import image_preprocessing as imgpr
 import feature_extraction as fextr
-
+import time
 # Extract label data from patients (either here or in seperate file)
-
 
 # Settings for feature extraction
 paramsPath = "Params.yaml"
 
-# dirPath = "../../patient_data"
-# imgpr.create_masks_and_nrrds(dirPath, overWrite=False)
+dirPath = "../../patient_data"
+t0 = time.time()
+imgpr.create_masks_and_nrrds(dirPath, overWrite=False)
+t1 = time.time()
 
-dirMaskPath = "../../patient_data/Pat14/Pat14T2M_mask"
+print('Elapsed time:', t1-t0)
+
+# dirMaskPath = "../../patient_data/Pat14/Pat14T2M_mask"
 # imgpr.create_3d_nrrd(dirMaskPath)
-dirImgPath = "../../patient_data/Pat14/Pat14T2U"
+# dirImgPath = "../../patient_data/Pat14/Pat14T2U"
 # imgpr.create_3d_nrrd(dirImgPath)
 
-results = fextr.extract_features_from_image(dirImgPath, dirMaskPath, paramsPath) 
-fextr.print_features(results)
+# results = fextr.extract_features_from_image(dirImgPath, dirMaskPath, paramsPath) 
+# fextr.print_features(results)
