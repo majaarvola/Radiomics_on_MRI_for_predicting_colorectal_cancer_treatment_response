488a0e908365e4f484247dc9ea17e67f97766e86
emil96aberg <emil.aberg96@gmail.com>
2020-11-18 10:27:49 +0100

    Update image_preprocessing.py
    Added code for creating masks without interior for files listed in manual_masks.txt. Showresult removed in create_mask

--- image_preprocessing.py
+++ image_preprocessing.py
@@ -6,25 +6,24 @@ import cv2
 import os
 import re
 
-def create_mask(imageFile, maskFile, showResult=False, addInterior=True):
+def create_mask(imagePath, maskPath, addInterior=True):
     """ 
     ACTION: 
         Generates a black and white mask of the input image
         The white area corresponds to green markings in the
         file including any interior points and the rest is black.
     INPUTS: 
-        imageFile: path to image file
-        maskFile: path of mask file to be created
-        showResult: display image, mask, segmented image
+        imagePath: path to image file
+        maskPath: path of mask file to be created
     OUTPUT: 
         1 if mask was created, 0 if not
     """
 
     # Read image (if it exists) and make copy for comparison
-    if cv2.haveImageReader(imageFile):
-        originalImage = cv2.imread(imageFile)
+    if cv2.haveImageReader(imagePath):
+        originalImage = cv2.imread(imagePath)
     else:
-        print(f"Failed to read input file at {imageFile}")
+        print(f"Failed to read input file at {imagePath}")
         return 0
     image = originalImage.copy()
     image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
@@ -42,17 +41,9 @@ def create_mask(imageFile, maskFile, showResult=False, addInterior=True):
         mask = erosion(mask) # Perform erosion on mask
 
     # save the output
-    if not cv2.imwrite(maskFile, mask):
-        print(f"Failed to write output file at {maskFile}")
+    if not cv2.imwrite(maskPath, mask):
+        print(f"Failed to write output file at {maskPath}")
 
-    if showResult:
-        # Apply mask to original image and show images for visual verification
-        result = cv2.bitwise_and(originalImage, originalImage, mask=mask)
-        cv2.imshow('intial', originalImage)
-        cv2.imshow('mask', mask)
-        cv2.imshow('result', result)
-        cv2.waitKey(0)
-    
     return 1
 
 def erosion(mask):
@@ -70,26 +61,26 @@ def erosion(mask):
     return cv2.erode(mask,kernel,iterations = 1)
 
 
-def create_3d_nrrd(folderPath):
+def create_3d_nrrd(dataPath):
     """
     ACTION: 
         Create three dimensional nrrd-file from all tiff-files in given folder, 
         the nrrd-file will be located next to the folder with the same name
     INPUTS: 
-        folderPath: folder with tiff-files
+        dataPath: folder with tiff-files
     OUTPUT: 
         1 if nrrd-file was created, 0 if not
     """
 
-    if not os.path.isdir(folderPath): return 0 # Exit if folder does not exist
+    if not os.path.isdir(dataPath): return 0 # Exit if folder does not exist
 
-    fileNames = os.listdir(folderPath) # Read content in the folder
+    fileNames = os.listdir(dataPath) # Read content in the folder
     tiffFileNames = [x for x in fileNames if x.endswith('.tiff')] # List with all tiff-images
     nrTiffFiles = len(tiffFileNames) # Number of tiff-images in folder
 
     if nrTiffFiles == 0: return 0 # Exit if folder contains no tiff-files
 
-    firstImage = cv2.imread(folderPath + '\\' + tiffFileNames[0], 0)
+    firstImage = cv2.imread(dataPath + '\\' + tiffFileNames[0], 0)
     w, h = firstImage.shape # Width and height of the first tiff-image in the folder
 
     # Make 3D-nrrd-file from all tiff-files in the folder
@@ -97,36 +88,36 @@ def create_3d_nrrd(folderPath):
     data = np.zeros((w,h,nrTiffFiles)) # Allocate 3D-array for data, 400x400 IMAGES ASSUMED
     for fileName in sorted(tiffFileNames):
         # Insert grayscaled layer into 3D-array
-        data[:,:,i] = cv2.imread(folderPath + '\\' + fileName, 0)
+        data[:,:,i] = cv2.imread(dataPath + '\\' + fileName, 0)
         i += 1
 
-    nrrd.write(folderPath + '.nrrd', data) # Write 3D-nrrd-file
+    nrrd.write(dataPath + '.nrrd', data) # Write 3D-nrrd-file
     return 1
 
-def create_masks_and_nrrds(folderPath, overWrite = False, readGray = True):
+def create_masks_and_nrrds(dataPath, overWrite = False, readGray = True):
     """ 
     ACTION: 
         Creates a mask for every tiff image in folder and subfolders (not for images with extension "_mask"). 
         Creates nrrd files for every tiff image and its mask.
         The masks are named with the same name as the original images but with an extension "_mask"
     INPUTS: 
-        folderpath
+        dataPath
         overwrite: overwrite existing masks and nrrd files
         readGray: create nrrd files based on gray scale image
     """
 
     # Read file names of manually created masks from manual_masks.txt
-    manualMasksPath = folderPath + '\\manual_masks.txt'
+    manualMasksPath = dataPath + '\\manual_masks.txt'
     with open(manualMasksPath, 'r') as manualMasksFile:
         manualMaskNames = manualMasksFile.read().splitlines()
 
-    patDirNames = os.listdir(folderPath)
+    patDirNames = os.listdir(dataPath)
     for patDirName in patDirNames:
 
         createdMasks = False
         createdNrrds = False
 
-        patDirPath = folderPath + '\\' + patDirName
+        patDirPath = dataPath + '\\' + patDirName
         if os.path.isdir(patDirPath):
 
             # This line will be reached once for every patient directory
@@ -150,7 +141,7 @@ def create_masks_and_nrrds(folderPath, overWrite = False, readGray = True):
                             imagePath = patSubDirPath + '\\' + fileNameExt
                             maskPath = patSubDirPath + '_mask\\' + fileName + '_mask' + fileExt
                             if not os.path.exists(maskPath) or (overWrite and fileName + '_mask' + fileExt not in manualMaskNames):
-                                createdMasks = create_mask(imagePath, maskPath, showResult=False) or createdMasks
+                                createdMasks = create_mask(imagePath, maskPath) or createdMasks
 
                     if not os.path.exists(patSubDirPath + '_mask.nrrd') or overWrite:
                         createdNrrds = create_3d_nrrd(patSubDirPath + '_mask') or createdNrrds
@@ -170,23 +161,23 @@ def create_masks_and_nrrds(folderPath, overWrite = False, readGray = True):
 
 
 
-def erosion_manual_masks(folderPath):
+def erosion_manual_masks(dataPath):
     """ 
     ACTION: 
         Perform erosion on all images listed in manual_masks.txt, overwrites the images with eroded ones.     
     INPUTS: 
-        folderpath
+        dataPath
     """
 
     # Read file names of manually created masks from manual_masks.txt
-    manualMasksPath = folderPath + '\\manual_masks.txt'
+    manualMasksPath = dataPath + '\\manual_masks.txt'
     with open(manualMasksPath, 'r') as manualMasksFile:
         manualMaskNames = manualMasksFile.read().splitlines()
 
-    patDirNames = os.listdir(folderPath)
+    patDirNames = os.listdir(dataPath)
     for patDirName in patDirNames:
 
-        patDirPath = folderPath + '\\' + patDirName
+        patDirPath = dataPath + '\\' + patDirName
         if os.path.isdir(patDirPath):
 
             # This line will be reached once for every patient directory
@@ -208,23 +199,28 @@ def erosion_manual_masks(folderPath):
                             mask = erosion(mask) # Perform erosion
                             cv2.imwrite(maskPath, mask) # Write
 
-def create_manual_masks(folderPath):
+def create_manual_masks(dataPath):
     """ 
     ACTION: 
         Perform erosion on all images listed in manual_masks.txt, overwrites the images with eroded ones.     
     INPUTS: 
-        folderpath
+        dataPath
     """
 
     # Read file names of manually created masks from manual_masks.txt
-    manualMasksPath = folderPath + '\\manual_masks.txt'
+    manualMasksPath = dataPath + '\\manual_masks.txt'
     with open(manualMasksPath, 'r') as manualMasksFile:
         manualMaskNames = manualMasksFile.read().splitlines()
 
-    patDirNames = os.listdir(folderPath)
+    # Remove '_mask' from manualMaskNames
+    manualMaskNames = [x.replace('_mask', '') for x in manualMaskNames if len(x)>3]
+
+    nrFoundFiles = 0 # Count the number of files found matching files in manualMaskNames
+
+    patDirNames = os.listdir(dataPath)
     for patDirName in patDirNames:
 
-        patDirPath = folderPath + '\\' + patDirName
+        patDirPath = dataPath + '\\' + patDirName
         if os.path.isdir(patDirPath):
 
             # This line will be reached once for every patient directory
@@ -232,7 +228,7 @@ def create_manual_masks(folderPath):
             for patSubDirName in patSubDirNames:
 
                 patSubDirPath = patDirPath + '\\' + patSubDirName
-                if os.path.isdir(patSubDirPath) and re.search('^Pat.+T2M[+frisk]*_mask$', patSubDirName): 
+                if os.path.isdir(patSubDirPath) and re.search('^Pat.+T2M[+frisk]*$', patSubDirName): 
                     # This line will be reached once for sub-directory starting with 'Pat',
                     # containing 'T2M', 'T2M+' or 'T2Mfrisk' and ending with '_mask'
 
@@ -241,7 +237,13 @@ def create_manual_masks(folderPath):
                     for fileNameExt in fileNameExts:
                         if fileNameExt in manualMaskNames:
                             # This line will be reached for files listed in manual_masks.txt
-                            maskPath = patSubDirPath + '\\' + fileNameExt
-                            mask = cv2.imread(maskPath) # Read
-                            mask = erosion(mask) # Perform erosion
-                            cv2.imwrite(maskPath, mask) # Write
+                            fileName, fileExt = os.path.splitext(fileNameExt)
+                            imagePath = patSubDirPath + '\\' + fileNameExt
+                            maskPath = patSubDirPath + '_mask\\' + fileName + '_mask' + fileExt
+                            create_mask(imagePath, maskPath, addInterior=False) # Creata mask without interior
+                            nrFoundFiles += 1
+
+    if not nrFoundFiles == len(manualMaskNames):
+        print('WARNING: Number of files found with names from \'manual_masks.txt\' does not match the number of files listed in \'manual_masks.txt\'.')
+        print('Number of files found: ', nrFoundFiles)
+        print('Number of listed files:', len(manualMaskNames))
