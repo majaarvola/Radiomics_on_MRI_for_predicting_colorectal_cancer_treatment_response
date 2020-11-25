1880a7e0626f29be2b057148ee248dfa03ad19c7
emil96aberg <emil.aberg96@gmail.com>
2020-11-25 17:02:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import image_preprocessing as imgpr
-import feature_extraction as fextr
+# import image_preprocessing as imgpr
+# import feature_extraction as fextr
+import feature_selection as fesel
 import time
 # Extract label data from patients (either here or in seperate file)
 
@@ -7,15 +8,18 @@ import time
 paramsPath = "Params.yaml"
 
 dataPath = "../../patient_data"
-featuresPath = "../../patient_data/selection_features.csv"
+selectionFeaturesPath = "../../patient_data/selection_features.csv"
 manualFeaturesPath = "../../patient_data/manual_features.csv"
 
 img2use = ["T2"]
 mask2use = ["M"]
 
-t0 = time.time()
+params = {'nFeatures': 10, 'internalFEMethod': 'MID'}
 
+t0 = time.time()
+selectedFeatures = fesel.select_features('MRMR', params, selectionFeaturesPath, manualFeaturesPath)
 t1 = time.time()
 
+print(selectedFeatures)
 
-print('Elapsed time', t1-t0)
+print('Elapsed time:', t1-t0)
