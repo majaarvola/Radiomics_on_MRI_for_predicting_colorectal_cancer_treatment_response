f73354772fa5716dd58a232f333e9e672480aa54
emil96aberg <emil.aberg96@gmail.com>
2020-12-09 12:09:35 +0100

    Update feature_selection.py
    Implemented LASSO

--- feature_selection.py
+++ feature_selection.py
@@ -1,6 +1,9 @@
+import numpy as np
 import pandas as pd
 import pymrmr
 from sklearn.preprocessing import KBinsDiscretizer
+from sklearn.feature_selection import SelectFromModel
+from sklearn.linear_model import LassoCV
 
 def select_features(method, params, selectionFeaturesPath, manualFeaturesPath): 
     """
@@ -20,6 +23,12 @@ def select_features(method, params, selectionFeaturesPath, manualFeaturesPath):
     X = pd.read_csv(selectionFeaturesPath, index_col=0, delimiter=';') # All data in selectionFeatures.csv
     y = pd.read_csv(manualFeaturesPath, index_col=0, delimiter=';') # All data in manualFeatures.csv
 
+    # Remove test data before doing feature selection
+    testIds = [1, 8, 13, 20, 40, 44, 49, 55]
+    trainIds = [v for v in X.index.values if v not in testIds]
+    y = y.loc[trainIds]
+    X = X.loc[trainIds]
+
     # Drop useless information
     X_diagnostics = [col for col in X if col.startswith('diagnostics')]
     X = X.drop(columns=X_diagnostics) # Data in selectionFeatures.csv, excluding diagnostic features
@@ -37,6 +46,23 @@ def select_features(method, params, selectionFeaturesPath, manualFeaturesPath):
         X = discretization(X, nBins, discStrategy) # Discretizing data
         XandY = pd.merge(y, X, how='inner', on='id') # Merging input and output into one DataFrame
         return pymrmr.mRMR(XandY, internalFEMethod, nFeatures) # Run MRMR
+
+    if method == 'LASSO': 
+        # Extract parameter setting, fit LASSO model and collect the importance of each feature
+        nFeatures = params['nFeatures']
+        clf = LassoCV(normalize=True, max_iter=2000).fit(X, y.values.ravel())
+        importance = np.abs(clf.coef_)
+
+        # Check that the features selected will have non-zero weight
+        nrNonZero = np.count_nonzero(importance)
+        if nrNonZero < nFeatures:
+            print(f'Warning: Number of features selected with LASSO is reduced from {nFeatures} to {nrNonZero} to only select features with non-zero weights. ')
+            nFeatures = nrNonZero
+        
+        # Select and return the best features (as string names)
+        idxFeatures = (-importance).argsort()[:nFeatures]
+        return [v for v in X.columns.values[idxFeatures]]
+        
     else:
         print(f'Method "{method}" is not implemented in feature_selection.py')
 
